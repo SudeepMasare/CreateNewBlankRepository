823900fa7a136796f8686365085ee3c57f977737
smasare <smasare@DIN56003294.corp.capgemini.com>
2017-06-22 11:01:04 +0530

    Adding GradleSonarQube Project

---
+++ GradleSonarQube/src/main/java/com/gradle/sonar/qube/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.gradle.sonar.qube;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To Sound Gradle Sonar Cube");
+		System.out.println("India");
+	}
+
+}
